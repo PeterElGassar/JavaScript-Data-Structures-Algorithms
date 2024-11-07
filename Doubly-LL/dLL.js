c45dea2519069b9e944d1ebe4fbf4b1095f7ba6d
peter nagy <peter951nagy@email.com>
2024-11-07 19:33:10 +0200

    add push $ pop method to doubly Linked List

---
+++ Doubly-LL/dLL.js
@@ -0,0 +1,56 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    else if (this.length === 1) {
+      this.makeEmpty();
+    } else {
+      let temp = this.tail;
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+      this.length--;
+      return temp;
+    }
+  }
+}
+
+// test code
+
+let dLL = new DoublyLinkedList(1);
+dLL.push(2);
+dLL.push(3);
